82e6918125d4839fb70f49b25130512fe80dc5b0
Matt Karl <matt@cloudkid.com>
2014-11-24 15:58:23 -0500

    Small dependency updates

--- tasks/version.js
+++ tasks/version.js
@@ -3,16 +3,7 @@ module.exports = {
 		files : {
 			'springroll.json' : 'version',
 			'bower.json' : 'version',
-			'deploy/index.html' : function(contents, version){
-				return contents.replace(
-						/src\=(\"|\')([^\?\n\r\"\']+)(\?v\=[a-z0-9\.]*)?(\"|\')/ig, 
-						'src="$2?v='+version+'"'
-					)
-					.replace(
-						/href\=(\"|\')([^\?\n\r\"\']+\.css)(\?v\=[a-z0-9\.]*)?(\"|\')/ig, 
-						'href="$2?v='+version+'"'
-					);
-			}
+			'deploy/index.html' : 'cache-bust'
 		}	
 	}
 };

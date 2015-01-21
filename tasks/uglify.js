44a9d12bf75dcdd8a3130be7f75de4915bc2c72d
Matt Karl <matt@cloudkid.com>
2015-01-21 16:12:39 -0500

    Excluding new Debug methods from uglify release

--- tasks/uglify.js
+++ tasks/uglify.js
@@ -17,7 +17,34 @@ module.exports = {
 				},
 				dead_code: true,
 				drop_console: true,
-				pure_funcs: ['include', 'Debug.log', 'Debug.info', 'Debug.warn', 'Debug.debug', 'Debug.error']
+				pure_funcs: [
+					'include',
+					'Debug.log',
+					'Debug.info',
+					'Debug.warn',
+					'Debug.debug',
+					'Debug.error',
+					'Debug.group',
+					'Debug.dir',
+					'Debug.trace',
+					'Debug.groupEnd',
+					'Debug.groupCollapsed',
+					'Debug.navy',
+					'Debug.blue',
+					'Debug.aqua',
+					'Debug.teal',
+					'Debug.olive',
+					'Debug.green',
+					'Debug.lime',
+					'Debug.yellow',
+					'Debug.orange',
+					'Debug.red',
+					'Debug.pink',
+					'Debug.purple',
+					'Debug.maroon',
+					'Debug.silver',
+					'Debug.gray'
+				]
 			}
 		}
 	}

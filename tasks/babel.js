306ca9b6603ec62087c42e3e44460f50debd7b07
David Lu <david.lu@twobulls.com>
2018-04-30 17:28:02 -0400

    add TODO for hardcoded env

--- tasks/babel.js
+++ tasks/babel.js
@@ -1,6 +1,7 @@
 module.exports = {
     options: {
         sourceMap: true,
+        // TODO: probably should put env config in springroll.json instead of hardcoding it
         presets: [[require.resolve('babel-preset-env'), { targets: { browsers: ['ie 11', 'safari 9'] }}], require.resolve('babel-preset-react')]
     },
     dist: {

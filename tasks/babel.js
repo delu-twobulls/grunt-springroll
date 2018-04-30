1c39c5e4cd2bbc97d76f020aacded5f0739d20ad
David Lu <david.lu@twobulls.com>
2018-04-30 17:26:16 -0400

    fix ie11 target

--- tasks/babel.js
+++ tasks/babel.js
@@ -1,7 +1,7 @@
 module.exports = {
     options: {
         sourceMap: true,
-        presets: [[require.resolve('babel-preset-env'), { targets: { browsers: ['ie11', 'safari 9'] }}], require.resolve('babel-preset-react')]
+        presets: [[require.resolve('babel-preset-env'), { targets: { browsers: ['ie 11', 'safari 9'] }}], require.resolve('babel-preset-react')]
     },
     dist: {
         files: {
